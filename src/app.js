8f5e1ffa08e383c28dbb830251349290084202d3
felipe29cl <fcarrascoa29@gmail.com>
2022-09-27 15:45:33 +0000

    project excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function aleatorio() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  var getNumberWho = Math.floor(Math.random() * who.length);
+  var getNumberWhat = Math.floor(Math.random() * what.length);
+  var getNumberWhen = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var text = [
+    who[getNumberWho] + " " + what[getNumberWhat] + " " + when[getNumberWhen]
+  ];
+
+  document.getElementById("demo").innerHTML = text;
+}
